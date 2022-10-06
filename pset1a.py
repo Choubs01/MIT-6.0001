ae05ad26bd41d1c95c7dd4016ce644efdaf758d5
Choubs01 <115124294+Choubs01@users.noreply.github.com>
2022-10-06 13:11:05 +1100

    Update pset1a.py

--- pset1a.py
+++ pset1a.py
@@ -1,3 +1,4 @@
+#Prints how many months it would take to save up for an intended amount according to salary and saving goal inputs, and other factors
 annual_salary = float(input("Enter your annual salary: "))
 portion_saved = float(input("Enter the percent of your salary to save, as a decimal: "))
 total_cost = float(input("Enter the cost of your dream home: "))
